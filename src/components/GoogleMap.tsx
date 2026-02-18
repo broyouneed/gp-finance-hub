bafc2a40ae8ebe12a744b861a3da9ec2f4576256
Aiswaryasurendran7 <aiswaryasurendran97@gmail.com>
2026-02-18 11:38:28 +0530

    map

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -129,15 +129,7 @@ export const GoogleMap = () => {
             }}
             className="lg:col-span-2 rounded-2xl overflow-hidden shadow-lg border-2 border-border h-[400px] lg:h-auto"
           >
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d291.12972244089474!2d76.21926853673698!3d10.532927474837988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7ee53dcc5bf4b%3A0x35aa9cca67f01daa!2sChembukkavu%20Shopping%20Complex!5e1!3m2!1sen!2sin!4v1768984562578!5m2!1sen!2sin"
-              width="100%"
-              height="100%"
-              style={{ border: 0, minHeight: "400px" }}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="GP Finserve Office Location"
-            />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4658.030430755483!2d76.20886607503847!3d10.535912489598614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDMyJzA5LjMiTiA3NsKwMTInNDEuMiJF!5e1!3m2!1sen!2sin!4v1771394221446!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </motion.div>
         </div>
       </div>
